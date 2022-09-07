b277afb8c5c632d51739181d737a2e32de949e65
Renzo Garavito <rgaravitoc@unal.edu.co>
2022-09-06 23:20:03 -0500

    initial commit

---
+++ MascotaFeliz.App/MascotaFeliz.App.Persistencia/Migrations/20220906022520_AddedPersonaCreatedCedula.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MascotaFeliz.App.Persistencia.Migrations
+{
+    public partial class AddedPersonaCreatedCedula : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Cedula",
+                table: "Personas",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cedula",
+                table: "Personas");
+        }
+    }
+}
